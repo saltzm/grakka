366d2736dce6683e9cbb0420d10ce984caffc46e
saltzm <saltzm@gmail.com>
2014-03-12 23:38:00 +0100

    added lognormal graph generator

--- src/main/scala/GraphGenerator.scala
+++ src/main/scala/GraphGenerator.scala
@@ -3,19 +3,47 @@ import scala.util.Random
 import Graph._
 import GraphMessages._
 
+
 object GraphGenerator {
+  private def uniform (mean: Int) = rand.nextInt(mean*2 + 1)
+  private def lognormal (mean: Double, stdDev: Double) = 
+    math.exp(mean + rand.nextGaussian() * stdDev).toInt
+  private val rand = new Random
+
   def generateRandomGraph (graph: ActorRef, nVertices: Int, avDegree: Int,
     nLabels: Int, nEdgeTypes: Int): Unit = {
+    var nEdges = 0
     graph ! StartLoading
-    val rand = new Random
     (0 until nVertices).foreach { i => 
-      val children = (0 until rand.nextInt(avDegree*2)).map (_ => 
+      val deg = uniform(avDegree)
+      nEdges += deg
+      val children = (0 until deg).map (_ => 
         (rand.nextInt(nVertices).toString.asInstanceOf[Id],
           Edge(rand.nextInt(nEdgeTypes).toString))
       ).toSet
       graph ! AddVertex(Vertex(i.toString, Map("label" ->
         rand.nextInt(nLabels).toString), children))
     }
+    println(s"nEdges: $nEdges")
     graph ! StopLoading
   }
+
+  def generateLognormalGraph (graph: ActorRef, nVertices: Int, mean: Double,
+    stdDev: Double, nLabels: Int, nEdgeTypes: Int): Unit = {
+    graph ! StartLoading
+    var nEdges = 0
+    (0 until nVertices).foreach { i => 
+      val deg = lognormal(mean, stdDev)
+      nEdges += deg
+      val children = (0 until deg).map (_ => 
+        (rand.nextInt(nVertices).toString.asInstanceOf[Id],
+          Edge(rand.nextInt(nEdgeTypes).toString))
+      ).toSet
+      graph ! AddVertex(Vertex(i.toString, Map("label" ->
+        rand.nextInt(nLabels).toString), children))
+    }
+    println(s"nEdges: $nEdges")
+    graph ! StopLoading
+  }
+
 }
--- src/main/scala/GraphRunner.scala
+++ src/main/scala/GraphRunner.scala
@@ -9,16 +9,19 @@ object GraphRunner extends Application {
   val graph = system.actorOf(Props[GraphActor], "graph")
 
   // Create random graph, adding vertices to the graph through message-passing
-  val (graphSize, avDegree, nLabels, nEdgeTypes) = (200000, 20, 30, 2)
-  GraphGenerator.generateRandomGraph(graph, graphSize, avDegree, nLabels, nEdgeTypes)
+  //val (graphSize, avDegree, nLabels, nEdgeTypes) = (200000, 20, 30, 2)
+  //GraphGenerator.generateRandomGraph(graph, graphSize, avDegree, nLabels, nEdgeTypes)
+  val (graphSize, mean, stdDev, nLabels, nEdgeTypes) = (200000, 2, 1.3, 10, 2)
+  GraphGenerator.generateLognormalGraph(graph, graphSize, mean, stdDev, nLabels,
+    nEdgeTypes)
 
   // Create path query
   val query = List(
     (Vertex("1", Map("label" -> "1"), Set()), Edge("0")),
     (Vertex("2", Map("label" -> "2"), Set()), Edge("1")),
-    (Vertex("3", Map("label" -> "3"), Set()), Edge("1")),
-    (Vertex("4", Map("label" -> "4"), Set()), Edge("1")),
-    (Vertex("5", Map("label" -> "5"), Set()), Edge("0"))
+    (Vertex("3", Map("label" -> "3"), Set()), Edge("1"))/*,*/
+    //(Vertex("4", Map("label" -> "4"), Set()), Edge("1")),
+    /*(Vertex("5", Map("label" -> "5"), Set()), Edge("0"))*/
   )
 
 
